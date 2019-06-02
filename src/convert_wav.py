fc9804e24d7f94e322acfed6c63c395884c71666
carlos <carlos.amds@gmail.com>
2019-06-02 17:32:44 -0300

    Return error if there's no mp3 folder and creates the output folder if it doesn't exist

--- src/convert_wav.py
+++ src/convert_wav.py
@@ -1,11 +1,17 @@
 from pydub import AudioSegment
 from glob import iglob
+import os
 
 DATA_FILES_MP3 = 'songs'
 DATA_FILES_WAV = 'songs_wav'
 
 
 def convert_mp3_to_wav():
+    if not os.path.exists(DATA_FILES_MP3):
+        print('The mp3 folder wasnt found')
+        return None
+    if not os.path.exists(DATA_FILES_WAV):
+        os.makedirs(DATA_FILES_WAV)
     index = 0
     for file in iglob(DATA_FILES_MP3 + '/*.mp3'):
         mp3_to_wav = AudioSegment.from_mp3(file)
