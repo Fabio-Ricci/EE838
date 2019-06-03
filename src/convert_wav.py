cff2fe38af0f57dd1b887604af5e6ac9425e09b3
Fabio-Ricci <fabioricci9@gmail.com>
2019-06-03 19:32:13 -0300

    convert wav to mp3

--- src/convert_wav.py
+++ src/convert_wav.py
@@ -4,6 +4,7 @@ import os
 
 DATA_FILES_MP3 = 'songs'
 DATA_FILES_WAV = 'songs_wav'
+DATA_FILES_MP3_CONVERTED = 'songs_converted'
 
 
 def convert_mp3_to_wav():
@@ -18,10 +19,24 @@ def convert_mp3_to_wav():
         mp3_to_wav = mp3_to_wav.set_frame_rate(48000)
         mp3_to_wav.export(DATA_FILES_WAV + '/' +
                           str(index) + '.wav', format='wav')
-
-
         index += 1
         print("Processed", file)
 
 
+def convert_wav_to_mp3():
+    if not os.path.exists(DATA_FILES_WAV):
+        print('The wav folder wasnt found')
+        return None
+    if not os.path.exists(DATA_FILES_MP3_CONVERTED):
+        os.makedirs(DATA_FILES_MP3_CONVERTED)
+    index = 0
+    for file in iglob(DATA_FILES_WAV + '/*.wav'):
+        wav_to_mp3 = AudioSegment.from_wav(file)
+        wav_to_mp3 = wav_to_mp3.set_frame_rate(48000)
+        wav_to_mp3.export(DATA_FILES_MP3_CONVERTED + '/' +
+                          str(index) + '.mp3', format='mp3')
+        index += 1
+        print("Processed", file)
+
 convert_mp3_to_wav()
+convert_wav_to_mp3()
