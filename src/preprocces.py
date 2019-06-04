2ec847bcff9014d9e2e7901e2cb4db2f7a681323
João Vitor Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2019-06-04 00:24:50 -0300

    use all songs

--- src/preprocces.py
+++ src/preprocces.py
@@ -17,7 +17,7 @@ def preprocess_data():
     wav_arr_ch2 = []
 
     for f in file_arr:
-        if i == 50:
+        if i == 40:
             break
         i += 1
         audio_binary = tf.read_file(f)
