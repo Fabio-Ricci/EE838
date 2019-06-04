c4fea0202aaaa0fbb0ea79bb8bc5765f2cede682
João Vitor Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2019-06-03 22:30:49 -0300

    run for 1k epochs

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -99,7 +99,7 @@ else:
 callbacks_list = []#[checkpoint]
 
 
-for i in range(2): # 100 epochs = 0.56h = 34 min
+for i in range(100): # 100 epochs = 0.56h = 34 min
     initial_epoch = 0
     epochs = 50
     # Fit the model
