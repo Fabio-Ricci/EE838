8cbc729cd6e336f2fc5b51bd6aafbf268e42191b
João Vitor Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2019-06-04 09:42:09 -0300

    300 epochs

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -79,7 +79,7 @@ encoding_dim = 2800  # 32 floats -> compression of factor 24.5, assuming the inp
 load = False
 
 if load:
-    autoencoder = load_model('/content/gdrive/My Drive/models/v2/model-v1-50eps')
+    autoencoder = load_model('/content/gdrive/My Drive/models/v2/model-300eps')
 else:
     input_img = Input(shape=(12348,))
     encoded = Dense(8400, activation='relu')(input_img)
@@ -100,7 +100,7 @@ callbacks_list = []#[checkpoint]
 
 
 for i in range(100): # 100 epochs = 0.56h = 34 min
-    initial_epoch = 0
+    initial_epoch = 300
     epochs = 50
     # Fit the model
     history = autoencoder.fit(data, data,
