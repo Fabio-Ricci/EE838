336489dbfe6ea056998d2bba811c1f3ea04e641a
João Vitor Araki Gonçalves <joao.goncalves@movile.com>
2019-06-05 20:31:27 -0300

    start from 300

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -78,7 +78,7 @@ if __name__ == "__main__":
 
     if load:
         autoencoder = load_model(
-            '/content/gdrive/My Drive/models/v5/model-250eps')
+            '/content/gdrive/My Drive/models/v5/model-300eps')
         print("model loaded succesfully")
     else:
         input_img = Input(shape=(12348,))
@@ -113,7 +113,7 @@ if __name__ == "__main__":
         del(wav_arr_ch1, wav_arr_ch2, sample_rate)
         print(len(data[0]))
 
-        initial_epoch = 250
+        initial_epoch = 300
         epochs = 50
         # Fit the model
         history = autoencoder.fit(data, data,
