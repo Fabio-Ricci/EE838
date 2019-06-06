a463bf0db0df7cbf96e9aa8d3a35f3de041b11f1
João Vitor Araki Gonçalves <joao.goncalves@movile.com>
2019-06-06 13:57:00 -0300

    reducing sample size

--- src/preprocces.py
+++ src/preprocces.py
@@ -18,7 +18,7 @@ def preprocess_data(sess):
     wav_arr_ch2 = []
 
     for f in file_arr:
-        if i == 15:
+        if i == 10:
             break
         i += 1
         audio_binary = tf.read_file(f)
