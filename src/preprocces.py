fix normalization

src/preprocces.py
@@ -12,7 +12,7 @@ def normalize(v):
     min = np.amin(v)
     max = np.amax(v)
     if min == max:
-        return 0, min, max
+        return v, min, max
     return ((v - min) / (max - min)), min, max
 
 def unnormalize(v, min, max):