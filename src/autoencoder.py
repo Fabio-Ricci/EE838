737463fd0557f65225821dcc3be0b596626a6af2
João Vitor Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2019-06-08 22:56:32 -0300

    remove regularizers and bigger

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -95,7 +95,7 @@ if __name__ == "__main__":
 
         decoded = Dense(7000, activation='relu')(encoded)
         decoded = Dense(8400, activation='relu')(decoded)
-        decoded = Dense(12348, activation='relu')(decoded)
+        decoded = Dense(12348, activation='sigmoid')(decoded)
 
         autoencoder = Model(input_img, decoded)
         autoencoder = compile_model(autoencoder)
