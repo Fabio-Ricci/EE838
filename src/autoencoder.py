cfc87602e3f237a4966877e397b6b6e37cf7febb
João Vitor Araki Gonçalves <joao.goncalves@movile.com>
2019-06-10 15:08:20 -0300

    new extreme test

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -92,7 +92,7 @@ if __name__ == "__main__":
         encoded = Dense(12348, activation='relu')(input_img)
         encoded = Dense(7000, activation='relu')(encoded)
         encoded = Dense(3000, activation='relu')(encoded)
-        encoded = Dense(500, activation='relu')(encoded)
+        encoded = Dense(1, activation='relu')(encoded)
 
         decoded = Dense(3000, activation='relu')(encoded)
         decoded = Dense(7000, activation='relu')(decoded)
