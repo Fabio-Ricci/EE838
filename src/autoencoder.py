f79101fee63993f903e95396454c1dc7a39b9600
João Vitor Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2019-06-11 23:10:46 -0300

    preprocessed data

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -61,12 +61,12 @@ def create_graphs(history, name=''):
 
 if __name__ == "__main__":
 
-    wav_arr_ch1, wav_arr_ch2, sample_rate = preprocess_data()
+    wav_arr_ch1, wav_arr_ch2 = preprocess_data()
     wav_arr_ch1 = np.array(wav_arr_ch1)
     wav_arr_ch2 = np.array(wav_arr_ch2)
 
     data = np.concatenate((wav_arr_ch1, wav_arr_ch2), axis=1)
-    del(wav_arr_ch1, wav_arr_ch2, sample_rate)
+    del(wav_arr_ch1, wav_arr_ch2)
     print(len(data[0]))
     
 
--- src/preprocces.py
+++ src/preprocces.py
@@ -8,6 +8,7 @@ from sklearn.preprocessing import MinMaxScaler
 import pickle
 
 DATA_FILES_WAV = 'songs_wav'
+PREPROCESSED_DATA = 'preprocessed'
 SECTION_SIZE = 12348 // 2
 
 
@@ -24,7 +25,7 @@ def unnormalize(v, scaler):
     return scaler.inverse_transform(v[:, np.newaxis]).flatten()
 
 
-def preprocess_data():
+def prepare_preprocess_data():
     i = 0
     file_arr = list(iglob(DATA_FILES_WAV + '/*.wav'))
     sess = tf.Session()
@@ -37,9 +38,6 @@ def preprocess_data():
         song_wav_arr_ch1 = []
         song_wav_arr_ch2 = []
 
-        if i == 50:
-            break
-        i += 1
         audio_binary = tf.read_file(f)
         wav_decoder = decode_wav(
             audio_binary, desired_channels=2)
@@ -48,33 +46,56 @@ def preprocess_data():
              wav_decoder.audio])
         audio = np.array(audio)
         print("audio ready")
-
-        rfft0 = rfft(audio[:, 0])
-        rfft1 = rfft(audio[:, 1])
+        audios = [audio[i * SECTION_SIZE:(i + 1) * SECTION_SIZE]
+                for i in range((len(audio) + SECTION_SIZE - 1) // SECTION_SIZE)]
+        rfft0 = []
+        rfft1 = []
+        for a in audios:
+            rfft0.extend(rfft(a[:, 0]))
+            rfft1.extend(rfft(a[:, 1]))
+        rfft0 = np.array(rfft0)
+        rfft1 = np.array(rfft1)
+        print(rfft0)
+        print("rfft done")
         song_wav_arr_ch1, scaler = normalize(rfft0)
         song_wav_arr_ch2, scaler = normalize(rfft1)
+        print("scaling done")
         with open(f + '-0.pickle', 'wb') as handle:
             pickle.dump(song_wav_arr_ch1, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
         with open(f + '-1.pickle', 'wb') as handle:
             pickle.dump(song_wav_arr_ch2, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-        # print("reconstructing")
-        # for s1, s2 in zip(song_wav_arr_ch1, song_wav_arr_ch2):
-        #     if len(s1) != SECTION_SIZE:
-        #         print(len(s1))
-        #         print("wrong sample")
-        #         continue
-        #     wav_arr_ch1.append(s1)
-        #     wav_arr_ch2.append(s2)
-        print("Returning File: " + f)
-        print("sample rate", sample_rate)
+def preprocess_data():
+    i = 0
+    file_arr = list(iglob(PREPROCESSED_DATA + '/*.pickle'))
+    file_arr.sort()
 
-    print("Number of returned chuncks", len(wav_arr_ch1))
+    wav_arr_ch1 = []
+    wav_arr_ch2 = []
+    left_channel = True
+
+    for f in file_arr:
+        pickleFile = open(f, 'rb')
+        data = pickle.load(pickleFile)
+        for d in data:
+            if len(d) != SECTION_SIZE:
+                print("wrong sample size")
+                print(len(d))
+                continue
+            if left_channel:
+                left_channel = False
+                wav_arr_ch1.append(d)
+            else: 
+                left_channel = True
+                wav_arr_ch2.append(d)
+        
+
+    print("Number of returned chuncks", len(wav_arr_ch1), len(wav_arr_ch2))
 
     if len(wav_arr_ch1) <= 0:
         print('No data')
         print('Quitting')
         exit()
 
-    return wav_arr_ch1, wav_arr_ch2, sample_rate
+    return wav_arr_ch1, wav_arr_ch2
