c5f000c83c5e39e827e0aaf6ee9a7e622b53af9e
João Vitor Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2019-06-12 21:34:30 -0300

    continue

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -84,11 +84,11 @@ if __name__ == "__main__":
     # this is the size of our encoded representations
     # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
     encoding_dim = 2800
-    load = False
+    load = True
 
     if load:
         autoencoder = load_model(
-            '/content/gdrive/My Drive/models/v16/model-100eps')
+            '/content/gdrive/My Drive/models/v16/model-150eps')
         print("model loaded succesfully")
     else:
         input_img = Input(shape=(12348,))
@@ -110,7 +110,7 @@ if __name__ == "__main__":
     for i in range(100):  # 100 epochs = 0.56h = 34 min
         
 
-        initial_epoch = 0
+        initial_epoch = 150
         epochs = 50
         # Fit the model
         history = autoencoder.fit(data, data,
@@ -118,7 +118,8 @@ if __name__ == "__main__":
                                   batch_size=128,
                                   epochs=epochs,
                                   shuffle=True,
-                                  callbacks=callbacks_list)
+                                  callbacks=callbacks_list,
+                                  initial_epoch=initial_epoch)
 
         score = autoencoder.evaluate(data, data, verbose=0)
         print('Test loss:', score)
