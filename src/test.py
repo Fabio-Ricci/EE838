968458ec9d9488e8f99214a0cd1aecfcfe61b4fc
João Vitor Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2019-06-15 00:22:31 -0300

    .

--- src/test.py
+++ src/test.py
@@ -13,7 +13,7 @@ import numpy as np
 from autoencoder import compile_model, load_model
 import matplotlib.pyplot as plt
 
-autoencoder = load_model('models/model-50eps')
+autoencoder = load_model('models/model-100eps')
 
 file_arr = iglob('test/*.wav')
 sess = tf.Session()
@@ -61,15 +61,15 @@ for f in file_arr:
             continue
 
         merged = np.hstack((norm1, norm2))
-        # plt.plot(merged)
-        # plt.show()
+        plt.plot(merged)
+        plt.show()
         merged = np.reshape(merged, (1,12348))
         predicted = autoencoder.predict(merged)
         # predicted = merged
         
         splitted = np.hsplit(predicted[0], 2)
-        # plt.plot(predicted[0])
-        # plt.show()
+        plt.plot(predicted[0])
+        plt.show()
         channel1 = splitted[0]
         channel2 = splitted[1]
         print(ch1_song.shape)
