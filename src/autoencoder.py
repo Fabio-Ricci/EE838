0cdef103bbd9ffe2d8839dc004d8d2d4f6c8b06f
João Vitor Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2019-06-15 22:42:55 -0300

    relu

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -84,7 +84,7 @@ if __name__ == "__main__":
         input_img = Input(shape=(12348,))
         encoded = Dense(9000, activation='relu')(input_img)
 
-        encoded = Dense(6000, activation='sigmoid')(encoded)
+        encoded = Dense(6000, activation='relu')(encoded)
 
         decoded = Dense(9000, activation='relu')(encoded)
         decoded = Dense(12348, activation='sigmoid')(decoded)
