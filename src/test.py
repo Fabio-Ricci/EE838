3b273a0c8859ca058a5c25e7bd8718e7299f3054
João Vitor Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2019-06-23 23:03:56 -0300

    close file

--- src/test.py
+++ src/test.py
@@ -112,8 +112,7 @@ for f in file_arr:
         audio_arr, file_format='wav', samples_per_second=sample_rate)
 
     wav_file = sess.run(wav_encoder)
-    open('/content/gdrive/Team Drives/EE838/test_reconstructed/' + str(file_number) + ".wav", 'wb').write(wav_file)
+    f = open('/content/gdrive/Team Drives/EE838/test_reconstructed/' + str(file_number) + ".wav", 'wb').write(wav_file)
+    f.close()
     file_number += 1
 print('all done')
-
-time.sleep(15)
