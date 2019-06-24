be1fa2fb7265698168d6e9081909aae9a5162ea9
João Vitor Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2019-06-24 19:04:58 -0300

    1500

--- src/test.py
+++ src/test.py
@@ -15,7 +15,7 @@ import time
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
 
-autoencoder = load_model('/content/gdrive/Team Drives/EE838/models/v25/model-1450eps')
+autoencoder = load_model('/content/gdrive/Team Drives/EE838/models/v25/model-1500eps')
 
 file_arr = iglob('/content/gdrive/Team Drives/EE838/test/*.wav')
 sess = tf.Session()
